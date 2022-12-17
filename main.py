4d6663c1f6c03e329a703114e0924d9d7696380a
ashleycelinaanil <ashleycelinaanilhere@gmail.com>
2022-12-17 12:51:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,16 @@ import streamlit as st
 
 
 st.title('RupeeX')
+<<<<<<< Updated upstream
 
 
 inputnumber = st.number_input('INPUT CURRENCY')
 st.write('The current value is ', inputnumber)
 outputnumber = st.number_input('OUTPUT CURRENCY')
 st.write('The current value is ', outputnumber)
+=======
+input_option = st.selectbox('Enter current currency',
+    ('USD','GBP','EURO'))
+
+st.write('Current currency : ', input_option)
+>>>>>>> Stashed changes
