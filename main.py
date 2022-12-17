72bc7bfa46457f706b2a55fe1bb85ca45ccb88d3
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2022-12-17 18:14:26 +0530

    Started working on API

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 import streamlit as st
+import requests
+import json
+import sys
+from pprint import pprint
+
+
+url = "http://data.fixer.io/api/latest?access_key=b8Lk9wuGFpbazSoVuRVwOGYrmy9YijlG"
+inputcurrency = requests.get(url)
+inputcurrency2 = json.loads(inputcurrency)
+ix = inputcurrency2["rates"]
+
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 st.title('RupeeX')
+
 col1, col2= st.columns(2,gap="large")
 with col1:
             input_option = st.selectbox('Enter current currency',
-                ('USD','GBP','EURO'))
+                ({ix}))
             st.write('Current currency : ', input_option)
             inputnumber = st.number_input('INPUT CURRENCY')
             st.write('The current value is ', inputnumber)
