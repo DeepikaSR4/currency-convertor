e6cb43b4fcb50589fd782a6b9202962836d9689a
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2022-12-18 01:52:56 +0530

    Revert "Update main.py"
    This reverts commit 6b024d03c43f08c5d46e3e279bebe449b3fc39a6.

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ import requests
 import json
 import sys
 from pprint import pprint
+
+
+r= requests.get(url="http://data.fixer.io/api/latest?access_key=b8Lk9wuGFpbazSoVuRVwOGYrmy9YijlG&from=inputcurrencycode&to=outputcurrencycode&amount=inputcurrencyamount")
+outputcurrency = requests.get(r)
+outputcurrency2 = json.loads(outputcurrency)
+ix = outputcurrency2["rates"]
+
+
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 st.title('RupeeX')
+<<<<<<< Updated upstream
+=======
 st.text(" ")
 st.text(" ")
+>>>>>>> Stashed changes
 
-url = 'https://api.apilayer.com/fixer/convert?access_key="b8Lk9wuGFpbazSoVuRVwOGYrmy9YijlG"&to=output_option&from=input_option&amount=inputnumber&result=outputnumber'
-payload = {}
-headers= {
-  "apikey": "b8Lk9wuGFpbazSoVuRVwOGYrmy9YijlG"
-}
-response = requests.request("GET", url, headers=headers, data = payload)
-status_code = response.status_code
-result = response.text
-
-
-currency_list = ['AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'GBP', 'HKD', 'HRK', 'HUF', 'IDR', 'ILS','EUR','INR', 'ISK', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR']
 col1, col2= st.columns(2,gap="large")
 with col1:
-            input_option = st.selectbox('Enter current currency',(currency_list))
+            input_option = st.selectbox('Enter current currency',
+                ('USD', 'Rupee', 'KRW'))
             st.write('Current currency : ', input_option)
-
             inputnumber = st.number_input('INPUT CURRENCY')
             st.write('The current value is ', inputnumber)
 
@@ -33,11 +33,9 @@ with col2:
 
             output_option = st.selectbox(
                 'Converted Currency',
-                (currency_list))
+                ())
             st.write('Converted currency:', output_option)
             outputnumber = st.number_input('OUTPUT CURRENCY')
-            st.write('The current value is ',result)
-
-
+            st.write('The current value is ', outputnumber)
 
 
