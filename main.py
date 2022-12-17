e3ef68bdb5134498858b7da2d25d5960c13e1220
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2022-12-17 20:41:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,11 @@ import sys
 from pprint import pprint
 
 
-url = "http://data.fixer.io/api/latest?access_key=b8Lk9wuGFpbazSoVuRVwOGYrmy9YijlG"
-inputcurrency = requests.get(url)
-inputcurrency2 = json.loads(inputcurrency)
-ix = inputcurrency2["rates"]
+r= requests.get(url="http://data.fixer.io/api/latest?access_key=b8Lk9wuGFpbazSoVuRVwOGYrmy9YijlG&from=inputcurrencycode&to=outputcurrencycode&amount=inputcurrencyamount")
+outputcurrency = requests.get(r)
+outputcurrency2 = json.loads(outputcurrency)
+ix = outputcurrency2["rates"]
+
 
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -18,7 +19,7 @@ st.title('RupeeX')
 col1, col2= st.columns(2,gap="large")
 with col1:
             input_option = st.selectbox('Enter current currency',
-                ({ix}))
+                ('USD', 'Rupee', 'KRW'))
             st.write('Current currency : ', input_option)
             inputnumber = st.number_input('INPUT CURRENCY')
             st.write('The current value is ', inputnumber)
@@ -27,7 +28,7 @@ with col2:
 
             output_option = st.selectbox(
                 'Converted Currency',
-                ('USD', 'Rupee', 'KRW'))
+                ())
             st.write('Converted currency:', output_option)
             outputnumber = st.number_input('OUTPUT CURRENCY')
             st.write('The current value is ', outputnumber)
