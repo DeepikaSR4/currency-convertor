acf9689def24e95f2f024df2552c50427deeeaa4
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2022-12-17 13:10:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ st.write('The current value is ', inputnumber)
 
 
 
-outputnumber = st.number_input('OUTPUT CURRENCY')
-st.write('The current value is ', outputnumber)
 output_option = st.selectbox(
     'Converted Currency',
     ('USD', 'Rupee', 'KRW'))
-
 st.write('Converted currency:', output_option)
+outputnumber = st.number_input('OUTPUT CURRENCY')
+st.write('The current value is ', outputnumber)
+
 
