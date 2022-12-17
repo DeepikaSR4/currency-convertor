1696b767467b50c0e194a6464f9141b645b3f9e1
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2022-12-17 12:48:22 +0530

    Added inputs

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 
 st.title('RupeeX')
+
+
+inputnumber = st.number_input('INPUT CURRENCY')
+st.write('The current value is ', inputnumber)
+outputnumber = st.number_input('OUTPUT CURRENCY')
+st.write('The current value is ', outputnumber)
