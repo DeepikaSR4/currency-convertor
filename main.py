568497689d8467defdb16925b44128e253d08362
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2022-12-17 13:08:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import streamlit as st
 
 st.title('RupeeX')
 
-
-inputnumber = st.number_input('INPUT CURRENCY')
-st.write('The current value is ', inputnumber)
-outputnumber = st.number_input('OUTPUT CURRENCY')
-st.write('The current value is ', outputnumber)
 input_option = st.selectbox('Enter current currency',
     ('USD','GBP','EURO'))
-
 st.write('Current currency : ', input_option)
+inputnumber = st.number_input('INPUT CURRENCY')
+st.write('The current value is ', inputnumber)
 
+
+
+outputnumber = st.number_input('OUTPUT CURRENCY')
+st.write('The current value is ', outputnumber)
 output_option = st.selectbox(
     'Converted Currency',
     ('USD', 'Rupee', 'KRW'))
