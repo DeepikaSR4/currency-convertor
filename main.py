d87181dcf4c1f856fc83c73a0cab6c033c2a3bad
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2022-12-18 11:11:30 +0530

    API updation

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import requests
-import json
-import sys
+
 from pprint import pprint
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
@@ -10,17 +9,25 @@ st.title('RupeeX')
 st.text(" ")
 st.text(" ")
 
-url = 'https://api.apilayer.com/fixer/convert?access_key="b8Lk9wuGFpbazSoVuRVwOGYrmy9YijlG"&to=output_option&from=input_option&amount=inputnumber&result=outputnumber'
-payload = {}
-headers= {
-  "apikey": "b8Lk9wuGFpbazSoVuRVwOGYrmy9YijlG"
+currency_list = ['AUD', ' BGN', ' BRL', ' CAD', ' CHF', ' CNY', ' CZK', ' DKK', ' GBP', ' HKD', 'HRK', 'HUF', 'IDR', 'ILS','EUR','INR', 'ISK', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR']
+
+
+url = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert"
+
+querystring = {"from": input_option,"to": output_option,"amount":inputnumber}
+
+headers = {
+	"X-RapidAPI-Key": "50433c2987msh40a9b38598b4f4bp183a24jsnc259adef6df4",
+	"X-RapidAPI-Host": "currency-conversion-and-exchange-rates.p.rapidapi.com"
 }
-response = requests.request("GET", url, headers=headers, data = payload)
-status_code = response.status_code
-result = response.text
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+output = response.json()
+result=output['result']
 
 
-currency_list = ['AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'GBP', 'HKD', 'HRK', 'HUF', 'IDR', 'ILS','EUR','INR', 'ISK', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR']
+
+
 col1, col2= st.columns(2,gap="large")
 with col1:
             input_option = st.selectbox('Enter current currency',(currency_list))
@@ -28,6 +35,9 @@ with col1:
 
             inputnumber = st.number_input('INPUT CURRENCY')
             st.write('The current value is ', inputnumber)
+
+
+
 
 with col2:
 
@@ -37,6 +47,10 @@ with col2:
             st.write('Converted currency:', output_option)
             outputnumber = st.number_input('OUTPUT CURRENCY')
             st.write('The current value is ',result)
+
+
+
+
 
 
 
