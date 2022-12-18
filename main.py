a10f89ad60a4618208beea9ae732d1785e623c90
DeepikaSR4 <deepikasanathanan1234@gmail.com>
2022-12-18 14:50:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,20 +12,19 @@ st.text(" ")
 currency_list = ['AUD', ' BGN', ' BRL', ' CAD', ' CHF', ' CNY', ' CZK', ' DKK', ' GBP', ' HKD', 'HRK', 'HUF', 'IDR', 'ILS','EUR','INR', 'ISK', 'JPY', 'KRW', 'MXN', 'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD', 'THB', 'TRY', 'USD', 'ZAR']
 
 
-# url = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert"
+url = "https://currency-conversion-and-exchange-rates.p.rapidapi.com/convert"
 
-# querystring = {"from": input_option,"to": output_option,"amount":inputnumber}
+#querystring = {'from': 'input_option','to': 'output_option','amount':'inputnumber'}
+querystring = {'from': 'USD','to': 'INR','amount':'1'}
+headers = {
+	"X-RapidAPI-Key": "50433c2987msh40a9b38598b4f4bp183a24jsnc259adef6df4",
+	"X-RapidAPI-Host": "currency-conversion-and-exchange-rates.p.rapidapi.com"
+} 
 
-# headers = {
-# 	"X-RapidAPI-Key": "50433c2987msh40a9b38598b4f4bp183a24jsnc259adef6df4",
-# 	"X-RapidAPI-Host": "currency-conversion-and-exchange-rates.p.rapidapi.com"
-# }
-
-# response = requests.request("GET", url, headers=headers, params=querystring)
-# output = response.json()
-# result=output['result']
-
-
+response = requests.request("GET", url, headers=headers, params=querystring)
+output = response.json()
+result = (output['result'])
+print(result)
 
 
 col1, col2= st.columns(2,gap="large")
@@ -46,12 +45,13 @@ with col2:
                 (currency_list))
             st.write('Converted currency:', output_option)
             outputnumber = st.number_input('OUTPUT CURRENCY')
-            st.write('The current value is ',outputnumber)
+            st.write('The current value is ',result)
 
   
 
 st.write('')
 st.write('Made by LUNE')
+
 
 
 
